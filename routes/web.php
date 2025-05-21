a4d6450cafb868506eb2f021f495e5bae7bbdd1d
Tatiana <tatianaoe83@gmail.com>
2025-05-20 20:41:24 -0600

    Access4

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/debug-auth', function () {
+    return auth()->check() ? auth()->user()->email : 'No autenticado';
+});
